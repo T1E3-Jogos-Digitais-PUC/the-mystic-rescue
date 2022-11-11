5619088abfc335bb7ca7ec210351ee29de48c680
mfreitas <mfreitas@blossomconsult.com>
2022-11-11 00:33:52 -0300

    Adicionada mecanica do nivel 2, modificado Menu (volume funcionando para música só nesta cena)

--- Assets/Codigo/Scripts/Cheats.cs
+++ Assets/Codigo/Scripts/Cheats.cs
@@ -6,53 +6,58 @@ using UnityEngine.SceneManagement;
 
 public class Cheats : MonoBehaviour
 {
-    private void Start()
-    {
+	private string message = "";
 
-    }
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.LeftAlt))
+		{
+			//Alt + I: Player Invencible
+			if (Input.GetKeyDown(KeyCode.I))
+			{
+				Debug.Log("Alt + I");
 
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.LeftAlt))
-        {
-            //Alt + I: Player Invencible
-            if (Input.GetKeyDown(KeyCode.I)) 
-            {
-                Debug.Log("Alt + I");
+				GameObject gameObj = GameObject.Find("PFPlayer");
+				Player player = gameObj.GetComponent<Player>();
 
-                GameObject gameObj = GameObject.Find("PFPlayer");
-                Player player = gameObj.GetComponent<Player>();
-                
-                if (player.IsInvencible)
-                {
-                    player.IsInvencible = false;
-                    Debug.Log("Player Is Invencible: Turned Off!");
-                }
-                else
-                {
-                    player.IsInvencible = true;
-                    Debug.Log("Player Is Invencible: Turned On!");
-                }
-            }
+				if (player.IsInvencible)
+				{
+					player.IsInvencible = false;
+					message = "Player Is Invencible: Turned Off!";
+				}
+				else
+				{
+					player.IsInvencible = true;
+					message = "Player Is Invencible: Turned On!";
+				}
+			}
 
-            //Alt + N: Load next Level
-            if (Input.GetKeyDown(KeyCode.N)) 
-            {
-                int scenesCount = SceneManager.sceneCountInBuildSettings;
-                Scene activeScene = SceneManager.GetActiveScene();
-                int index = activeScene.buildIndex;
-                Debug.Log($"Scene {index} of {scenesCount}");
+			//Alt + N: Load next Level
+			if (Input.GetKeyDown(KeyCode.N))
+			{
+				int scenesCount = SceneManager.sceneCountInBuildSettings;
+				Scene activeScene = SceneManager.GetActiveScene();
+				int index = activeScene.buildIndex;
 
-                if (index < scenesCount - 1)
-                {
-                    SceneManager.LoadScene(index + 1);
-                }
-                else
-                {
-                    SceneManager.LoadScene(0);
-                }
-            }
-        }
-        
-    }
+				if (index < scenesCount - 1)
+				{
+					message = $"From scene {index} of {scenesCount} to scene {index + 1} of {scenesCount}";
+					SceneManager.LoadScene(index + 1);
+				}
+				else
+				{
+					message = $"From scene {index} of {scenesCount} to scene 0 of {scenesCount}";
+					SceneManager.LoadScene(0);
+				}
+			}
+
+
+
+            if (message != "") Debug.Log(message);
+		}
+
+	}
+
+
+
 }
