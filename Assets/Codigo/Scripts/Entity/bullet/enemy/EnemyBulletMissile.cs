589fb5f5956c32ff963a554b172c095604b51e78
rafarvns <rafarvns@gmail.com>
2022-11-13 23:54:13 -0300

    Adiciona novos inimigos

---
+++ Assets/Codigo/Scripts/Entity/bullet/enemy/EnemyBulletMissile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Codigo.Scripts.Entity.character;
+using UnityEngine;
+
+namespace Codigo.Scripts.Entity.bullet.enemy
+{
+    public class EnemyBulletMissile: Character
+    {
+        public float Acceleration = 0.01f;
+        private void Start()
+        {
+            Direction = new Vector3(-1, 0, 0);
+        }
+        void Update()
+        {
+            BulletMovement();
+        }
+
+        private void BulletMovement()
+        {
+            transform.position += Direction * (Speed * Time.deltaTime);
+            Speed += Acceleration;
+        }
+    }
+}
---
+++ Assets/Codigo/Scripts/Entity/enemy/stage1/EnemyWeaponLaser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Codigo.Scripts.Entity.character;
+using UnityEngine;
+
+namespace Codigo.Scripts.Entity.enemy.stage1
+{
+    public class EnemyWeaponLaser: MonoBehaviour
+    {
+        public bool Loop = false;
+        public float DelayToFirstShot = 0.0f;
+        public GameObject Prefab;
+        private Character ParentCharacter;
+        private float NextFire;
+        private bool CanShot;
+
+        private void Start()
+        {
+            ParentCharacter = Character.GetParentCharacterGameObject(gameObject);
+            NextFire = DelayToFirstShot;
+            CanShot = true;
+        }
+
+        void Update()
+        {
+            if (ParentCharacter && CanShot)
+            {
+                if (NextFire <= 0.0f)
+                {
+                    NextFire = ParentCharacter.FireRate;
+                    if (!Loop)
+                    {
+                        CanShot = false;
+                    }
+                }
+                else
+                {
+                    GameObject instantiatedBullet = Instantiate(Prefab, transform.position, ParentCharacter.transform.rotation);
+                    List<Character> childrenBullets = Character.GetChildrenCharacterGameObject(instantiatedBullet);
+                    foreach (var childrenBullet in childrenBullets)
+                    {
+                        childrenBullet.AttackDamage *= ParentCharacter.AttackDamage;
+                    }
+                }
+                NextFire -= Time.deltaTime;
+            }
+        }
+        
+    }
+}
